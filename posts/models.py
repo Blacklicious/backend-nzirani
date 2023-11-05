634fc980a7b7c69753d5e666bea3b80bab50c45e
blacklicious <djibou360@hotmail.com>
2023-11-05 22:32:11 +0000

    addition of thumbnails in posts

--- posts/models.py
+++ posts/models.py
@@ -26,10 +26,13 @@ class Article(CommonFields):
     file = models.ImageField(upload_to='images/publications/articles/')
 
 class Magazine(CommonFields):
+    thumbnail = models.ImageField(upload_to='pdf/publications/magazines/thumbnails/')
     file = models.FileField(upload_to='pdf/publications/magazines/', null=True, blank=True)
 
 class Podcast(CommonFields):
+    thumbnail = models.ImageField(upload_to='audio/publications/podcasts/thumbnails/')
     file = models.FileField(upload_to='audio/publications/podcasts/', null=True, blank=True)
 
 class Video(CommonFields):
+    thumbnail = models.ImageField(upload_to='video/publications/videos/thumbnails/')
     file = models.FileField(upload_to='video/publications/videos/', null=True, blank=True)
